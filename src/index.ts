1e57941f58a7521a434ddff015a75945757f1ec7
Marc Lammers <lammers.marc@gmail.com>
2020-04-04 17:42:49 +0200

    export all enums

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-export { ComfortCloudClient } from "./ComfortCloudClient"
-export { Device } from "./model/Device"
-export { Group } from "./model/Group"
-export * from "./domain/enums"
+export { ComfortCloudClient } from './ComfortCloudClient'
+export { Device } from './model/Device'
+export { Group } from './model/Group'
+export {
+  Power,
+  AirSwingAutoMode,
+  AirSwingLR,
+  AirSwingUD,
+  EcoMode,
+  FanAutoMode,
+  FanSpeed,
+  OperationMode,
+  dataMode,
+} from './domain/enums'
