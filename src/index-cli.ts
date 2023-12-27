40c7a57f4cfc9e6cdcd1bff3c2872b8fb652b2e1
Marc Lammers <marc@lammers.dev>
2023-12-27 21:02:19 +0100

    CLI script added

--- src/index-cli.ts
+++ src/index-cli.ts
@@ -5,14 +5,95 @@ import { Group } from './model/Group.js'
 import { Device } from './model/Device.js'
 
 import inquirer from 'inquirer'
+import select, { Separator } from '@inquirer/select'
+import { forEach } from 'lodash'
+import { group } from 'console'
+
+type Command = 'get-group' | 'get-device' | 'exit' | null
+
+let client: ComfortCloudClient
+
+async function SelectCommand(): Promise<Command> {
+  const nextCommand: Command = await select({
+    message: 'Select a package manager',
+    choices: [
+      {
+        name: 'Get groups',
+        value: 'get-group',
+        description: 'Get all groups for this account',
+      },
+      {
+        name: 'Get device',
+        value: 'get-device',
+        description: 'Get a device for a given guid',
+      },
+      {
+        name: 'Exit',
+        value: 'exit',
+        description: 'Exit script',
+      },
+    ],
+  })
+  return nextCommand
+}
+
+async function SelectDevice(selectedGroup: Group) {
+  console.log(`Found ${selectedGroup.devices.length} devices.`)
+  const choicesDevices = new Array()
+  for (let device of selectedGroup.devices) {
+    choicesDevices.push({
+      name: device.name,
+      value: device,
+    })
+  }
+  choicesDevices.push({
+    name: 'Print group',
+    value: selectedGroup,
+  })
+  const selectedObj: Device|Group = await select({
+    message: 'Select a device or print group',
+    choices: choicesDevices,
+  })
+
+  console.log(JSON.stringify(selectedObj, null, 2))
+}
+
+async function SelectGroup(): Promise<Group> {
+  var groups = await client.getGroups()
+  console.log(`Found ${groups.length} groups.`)
+  const choicesGroups = new Array()
+  for (let group of groups) {
+    choicesGroups.push({
+      name: group.name,
+      value: group,
+    })
+  }
+  const selectedGroup: Group = await select({
+    message: 'Select a group',
+    choices: choicesGroups,
+  })
+
+  return selectedGroup
+}
+
+async function GetDevice() {
+  const deviceGuid: string = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'Guid',
+      message: 'Guid of the device',
+    },
+  ])
+  const device = await client.getDevice(deviceGuid)
+  console.log(device, null, 2)
+}
 
 async function start() {
-  const answers = await inquirer
-  .prompt([
+  const answers = await inquirer.prompt([
     {
       type: 'input',
       name: 'username',
-      message: "Username",
+      message: 'Username',
     },
     {
       type: 'password',
@@ -23,10 +104,26 @@ async function start() {
   const username: string = answers.username
   const password: string = answers.password
 
-  const client = new ComfortCloudClient()
+  client = new ComfortCloudClient()
   await client.login(username, password)
-  const groups = await client.getGroups()
-  console.log(JSON.stringify(groups, null, '  '))
+  console.log('Login successful.')
+
+  
+  let nextCommand: Command = null
+  while(nextCommand != 'exit') {
+    nextCommand = await SelectCommand()
+    switch (nextCommand) {
+      case 'get-device':
+        await GetDevice()
+        break
+      case 'get-group':
+        const selectedGroup = await SelectGroup()
+        await SelectDevice(selectedGroup)
+        break
+      default:
+        break
+    }
+  }
 }
 
 start()
